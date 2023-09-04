cf729750ab3f693ade6db602f6ffb610820823e9
SharefaALEnezi <sharefa2009@icloud.com>
2023-09-04 19:35:22 +0300

    done
    .

---
+++ script.js
@@ -0,0 +1,22 @@
+alert ("مرحباً بك في موقعي! أنا شريفة، وسوف أحسب درجتك");
+let grade= prompt("please put your grade.");
+
+console.log(grade);
+if(grade>=90 && grade<=100){console.log
+("لقد حصلت على امتياز 🥳")
+}
+else if(grade>=80 && grade<= 89){console.log
+("لقد حصلت على جيد جداً🤩")
+}
+else if(grade>=70 && grade<= 79){console.log
+ ("لقد حصلت على جيد🙂")
+}
+else if(grade>=60 && grade<= 69){console.log
+ ("لقد حصلت على مقبول😕")
+}
+else if(grade>=50 && grade<= 59){console.log
+ ("لقد حصلت على ضعيف☹️")
+}
+else (grade>50); {console.log
+ ("راسب💔")
+}
